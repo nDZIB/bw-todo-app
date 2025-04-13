f8bfe2cc23ac5cceda6aeaf8e492a06c5025b0a5
nDZIB <brunoserkwi@gmail.com>
2025-04-13 16:26:51 +0100

    Enable cors
    Added cors config, to ensure frontend app communication with the api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,17 @@ import express from 'express'
 import taskRouter from './routes/task.routes';
 import Swagger  from 'swagger-ui-express';
 import swaggerDocument from '../docs/swagger.json';
+import cors from 'cors';
 import { AppDataSource } from './lib/typeorm/data-source';
 
 const PORT = process.env.PORT || 8080;
-
 const app = express()
+const allowedOrigins = ['http://localhost:4200'];
+
+
+app.use(cors({
+    origin: allowedOrigins
+}))
 app.use(express.json())
 
 AppDataSource.initialize()
