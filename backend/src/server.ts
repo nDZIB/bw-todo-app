6aa2b61b68d59539bee73d5e0da371e5f08cb349
nDZIB <brunoserkwi@gmail.com>
2025-04-13 21:59:08 +0100

    Add docker-compose run script
    - Refactored backend api to accomodote deployment to docker
    - Wrote dockerfiles for both frontend app and backend api
    - Added a docker-compose script to spin up the fullstack application and dependencies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,25 +2,38 @@ import 'reflect-metadata'
 
 import express from 'express'
 import taskRouter from './routes/task.routes';
-import Swagger  from 'swagger-ui-express';
-import swaggerDocument from '../docs/swagger.json';
+import Swagger from 'swagger-ui-express';
 import cors from 'cors';
 import { AppDataSource } from './lib/typeorm/data-source';
+import path from 'path';
+
+const swaggerPath = process.env.DEPLOYMENT_ENV
+    ? path.join(__dirname, './', 'swagger.json')
+    : path.join(__dirname, '..', 'docs', 'swagger.json');
+
+
 
 const PORT = process.env.PORT || 8080;
 const app = express()
 const allowedOrigins = ['http://localhost:4200'];
 
+AppDataSource.initialize()
 
 app.use(cors({
     origin: allowedOrigins
 }))
 app.use(express.json())
-
-AppDataSource.initialize()
-
 app.use('/api/v1/tasks', taskRouter)
-app.use("/docs", Swagger.serve, Swagger.setup(swaggerDocument))
+
+// configure swagger docs
+import(swaggerPath)
+    .then(module => {
+        const swaggerDocument = module.default;
+        app.use("/docs", Swagger.serve, Swagger.setup(swaggerDocument))
+    })
+    .catch(error => {
+        console.error('Error importing swagger.json:', error);
+    });
 
 app.listen(PORT, () => {
     console.log(`TODO API running at :${PORT}`)
